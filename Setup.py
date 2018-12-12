db7cc6adaaa36632f91a2b363096ea723a935c87
chiranjeevbitm <31815150+chiranjeevbitm@users.noreply.github.com>
2018-12-12 15:48:43 +0530

    Update Setup.py
    Run the existing code. The fan should operate when the
    surrounding temperature is greater than the threshold value in the sketch

--- Setup.py
+++ Setup.py
@@ -24,3 +24,12 @@ print (‘Getting data from the sensor’)
 humidity, temperature = Adafruit_DHT.read_retry(sensor,17)
 print ('Temp={0:0.1f}*C humidity={1:0.1f}%'.format(temperature, humidity))
 
+
+#Set the GPIO pin connected with the relay’s input pin as output in the sketch
+GPIO.setup(13,GPIO.OUT)
+#Set the relay pin high when the temperature is greater than 30 
+if temperature > 30:
+  GPIO.output(13,0) # Relay is active low
+  print(‘Relay is on')
+  sleep(5)
+  GPIO.output(13,1) # Relay is turned off after delay of 5 seconds
